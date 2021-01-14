b434936a2b6ca280d0f7d5a4977189601fc1f605
hoonchany99 <hoonchany99@gmail.com>
2021-01-15 04:44:18 +0900

    24

--- trydjango/products/views.py
+++ trydjango/products/views.py
@@ -4,18 +4,29 @@ from .models import Product
 from .forms import ProductForm
 # Create your views here.
 
-def product_create_view(request):
-	form = ProductForm(request.POST or None)
-	if form.is_valid():
-		form.save()
-		form = ProductForm()
-	context = {
-		# 'title': obj.title,
-		# 'description': obj.description
-		'form': form
 
-	}
+def product_create_view(request):
+	# print(request.GET)
+	# print(request.POST)
+	if request.method == "POST":
+		my_new_title = request.POST.get('title')
+		print(my_new_title)
+	# Product.objects.create(title=my_new_title)
+	context = {}
 	return render(request, "products/product_create.html" , context)
+
+# def product_create_view(request):
+# 	form = ProductForm(request.POST or None)
+# 	if form.is_valid():
+# 		form.save()
+# 		form = ProductForm()
+# 	context = {
+# 		# 'title': obj.title,
+# 		# 'description': obj.description
+# 		'form': form
+
+# 	}
+# 	return render(request, "products/product_create.html" , context)
 
 def product_detail_view(request):
 	obj = Product.objects.get(id = 1)
